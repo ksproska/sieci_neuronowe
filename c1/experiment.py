1e34235735df5e938237129a1865b96595ac44f1
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-10-09 16:44:24 +0200

    counts percentage matching

--- c1/experiment.py
+++ c1/experiment.py
@@ -37,18 +37,20 @@ if __name__ == '__main__':
     repetitions = 200
     for experiment_case in [(d_and, "and"), (d_or, "or"), (d_xor, "xor")]:
         for estimate_func in [(unipolar, "unipolar"), (bipolar, "bipolar")]:
-            plt.title(f'{experiment_case[1]} - {estimate_func[1]}')
             x = randomize(merge_multiple_times(x_original[estimate_func[0]], repetitions))
             plt.plot(x[1, :], x[2, :], marker="o", linestyle='None')
 
             d = merge_multiple_times(experiment_case[0][estimate_func[0]], repetitions)
             weights = get_random_weights(3)
             alfa = 0.1
+            y = None
             for i in range(100):
                 c = cost(weights, x.T)
                 y = estimate(c, lambda v: estimate_func[0](0, v))
                 dw = delta_w(d, y, x)
                 weights = calculate_new_weight(weights, alfa, dw)
+            plt.title(f'{experiment_case[1]} - {estimate_func[1]} - {np.mean(d == y)}')
+            print(f'{experiment_case[1]} - {estimate_func[1]} - {np.mean(d == y)}')
 
             x = np.linspace(-1, 1.5, 10)
             y = (-weights[1] * x - weights[0]) / weights[2]
