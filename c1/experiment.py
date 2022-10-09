bc87f401704b7f3fe66bbd96849c4be199ea926d
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-10-09 15:53:33 +0200

    for and or and xor displayed results

--- c1/experiment.py
+++ c1/experiment.py
@@ -2,37 +2,44 @@ from usefull import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-x = np.array(
+x_original = np.array(
     [
-    [1, 1, 1, 1],
-    [0, 0, 1, 1], 
-    [0, 1, 0, 1]
+        [1, 1, 1, 1],
+        [0, 0, 1, 1],
+        [0, 1, 0, 1]
     ]
 )
 
-d = np.array(
+d_and = np.array(
     [0, 0, 0, 1]
+)
+d_or = np.array(
+    [0, 1, 1, 1]
+)
+d_xor = np.array(
+    [0, 1, 1, 0]
 )
 
 if __name__ == '__main__':
     repetitions = 200
-    x = randomize(merge_multiple_times(x, repetitions))
-    plt.plot(x[1, :], x[2, :], marker="o", linestyle='None')
+    for d_name in [(d_and, "and"), (d_or, "or"), (d_xor, "xor")]:
+        plt.title(d_name[1])
+        x = randomize(merge_multiple_times(x_original, repetitions))
+        plt.plot(x[1, :], x[2, :], marker="o", linestyle='None')
 
-    d = merge_multiple_times(d, repetitions)
-    weights = get_random_weights(3)
-    alfa = 0.1
-    teta = 0
-    func = lambda x: unipolar(teta, x)
-    for i in range(100):
-        c = cost(weights, x.T)
-        y = estimate(c, func)
-        dw = delta_w(d, y, x)
-        weights = calculate_new_weight(weights, alfa, dw)
-        print(weights)
+        d = merge_multiple_times(d_name[0], repetitions)
+        weights = get_random_weights(3)
+        alfa = 0.1
+        teta = 0
+        func = lambda x: unipolar(teta, x)
+        for i in range(100):
+            c = cost(weights, x.T)
+            y = estimate(c, func)
+            dw = delta_w(d, y, x)
+            weights = calculate_new_weight(weights, alfa, dw)
+            print(weights)
 
-    x = np.linspace(0, 1.5, 10)
-    y = (-weights[1] * x - weights[0])/weights[2]
-    plt.plot(x, y)
-    plt.show()
+        x = np.linspace(-0.5, 1.5, 10)
+        y = (-weights[1] * x - weights[0]) / weights[2]
+        plt.plot(x, y)
+        plt.show()
