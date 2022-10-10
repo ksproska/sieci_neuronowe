800fc96104b1c98c5f2691325859fb0e73bb0678
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-10-10 08:18:39 +0200

    alfa vals added

--- c1/experiment.py
+++ c1/experiment.py
@@ -38,43 +38,42 @@ if __name__ == '__main__':
     test_percent = 0.25
     for experiment_case in [(d_and, "AND"), (d_or, "OR"), (d_xor, "XOR")]:
         for func_type in [(unipolar, "unipolar"), (bipolar, "bipolar")]:
-            alfa = 0.1
-            epoce_numb = 100
+            for alfa in [0.01, 0.1, 1]:
+                epoch_numb = 100
 
-            d_case = experiment_case[0]
-            estimate_func = func_type[0]
-            x_original = x_originals[estimate_func]
-            x_all = randomize(merge_multiple_times(x_original, repetitions))
-            d_all = merge_multiple_times(d_case[estimate_func], repetitions)
+                d_case = experiment_case[0]
+                estimate_func = func_type[0]
+                x_original = x_originals[estimate_func]
+                x_all = randomize(merge_multiple_times(x_original, repetitions))
+                d_all = merge_multiple_times(d_case[estimate_func], repetitions)
 
-            train_size = int(x_all.shape[1] - x_all.shape[1] * test_percent)
-            x_train, x_test = x_all[:, :train_size], x_all[:, train_size:]
+                train_size = int(x_all.shape[1] - x_all.shape[1] * test_percent)
+                x_train, x_test = x_all[:, :train_size], x_all[:, train_size:]
 
-            weights = get_random_weights(x_all.shape[0])
-            y = None
-            epoche_count = 0
-            for i in range(epoce_numb):
-                epoche_count += 1
-                c = cost(weights, x_train.T)
+                weights = get_random_weights(x_all.shape[0])
+                y = None
+                epoch_count = 0
+                for i in range(epoch_numb):
+                    epoch_count += 1
+                    c = cost(weights, x_train.T)
+                    y = estimate(c, lambda v: estimate_func(0, v))
+                    dw = delta_w(d_all[:train_size], y, x_train)
+                    weights = calculate_new_weight(weights, alfa, dw)
+                    if 1.0 - np.mean(d_all[:train_size] == y) < 0.01:
+                        break
+
+                c = cost(weights, x_test.T)
                 y = estimate(c, lambda v: estimate_func(0, v))
-                dw = delta_w(d_all[:train_size], y, x_train)
-                weights = calculate_new_weight(weights, alfa, dw)
-                if 1.0 - np.mean(d_all[:train_size] == y) < 0.01:
-                    break
+                diff = np.mean(d_all[train_size:] == y) * 100
 
-            c = cost(weights, x_test.T)
-            y = estimate(c, lambda v: estimate_func(0, v))
-            diff = np.mean(d_all[train_size:] == y) * 100
+                print(f'{experiment_case[1]}\t{func_type[1]}\t{epoch_count}\t{alfa}\t{diff}%')
 
-            print(f'{experiment_case[1]}\t{func_type[1]}\t{epoche_count}\t{diff}%')
+                plt.title(f'{experiment_case[1]} - {func_type[1]} - epochs: {epoch_count} - alfa: {alfa} - match: {diff}%')
+                plt.scatter(x_test[1, :], x_test[2, :])
 
-            plt.title(f'{experiment_case[1]} - {func_type[1]} - {epoche_count} - {diff}%')
-            plt.scatter(x_test[1, :], x_test[2, :])
-
-            x_all = np.linspace(-1.5 if estimate_func == bipolar else -0.5, 1.5, 10)
-            y = (-weights[1] * x_all - weights[0]) / weights[2]
-            plt.plot(x_all, y)
-            plt.axhline(y=0, color="k")
-            plt.axvline(x=0, color="k")
-            plt.grid(True)
-            plt.show()
+                x_all = np.linspace(-1.25 if estimate_func == bipolar else -0.25, 1.25, 10)
+                plt.plot(x_all, (-weights[1] * x_all - weights[0]) / weights[2])
+                plt.axhline(y=0, color="k")
+                plt.axvline(x=0, color="k")
+                plt.grid(True)
+                plt.show()
