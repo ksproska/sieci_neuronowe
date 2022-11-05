b9da732096916f01657bb810d0b5e15e442ac74a
Kamila Sproska <kamila.sproska@gmail.com>
2022-11-05 11:25:26 +0100

    saving figures to files

--- exercise2/multilayer/mlp.py
+++ exercise2/multilayer/mlp.py
@@ -1,14 +1,19 @@
+import datetime
 import random
+import time
+
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from keras.datasets import mnist
 from activation_functions import *
 from utils import *
+
 plt.style.use('ggplot')
 
 
 class MLP:
     def __init__(self, learning_rate, neurons_in_hidden_layers, activation_fun, output_classes_numb=10):
+        self.iterations = 0
         self.activation_fun = activation_fun
         self.activation_fun_derivative = {
             relu: relu_derivative,
@@ -22,10 +27,11 @@ class MLP:
         train_y = train_y.reshape((train_y.shape[0], -1))
         test_X = test_X.reshape((test_X.shape[0], -1))
         test_y = test_y.reshape((test_y.shape[0], -1))
-        (self.train_X, self.train_y), (self.test_X, self.test_y) = (train_X/255, train_y), (test_X/255, test_y)
+        (self.train_X, self.train_y), (self.test_X, self.test_y) = (train_X / 255, train_y), (test_X / 255, test_y)
 
         self.neurons_in_layers = [self.train_X.shape[1]] + neurons_in_hidden_layers + [output_classes_numb]
-        self.all_weights = [np.random.randn(*self.get_weights_matrix_shape(i)) for i in range(len(self.neurons_in_layers) - 1)]
+        self.all_weights = [np.random.randn(*self.get_weights_matrix_shape(i)) for i in
+                            range(len(self.neurons_in_layers) - 1)]
         self.all_bs = [random.random() for i in range(len(self.neurons_in_layers) - 1)]
         self.d = get_d_matrix(self.train_y, output_classes_numb)
         self.d_test = get_d_matrix(self.test_y, output_classes_numb)
@@ -34,6 +40,7 @@ class MLP:
         return [self.neurons_in_layers[first_inx + 1], self.neurons_in_layers[first_inx]]
 
     def count_one_step(self):
+        self.iterations += 1
         f = self.activation_fun
         f_prim = self.activation_fun_derivative
         x = self.train_X.T
@@ -99,20 +106,12 @@ class MLP:
         return f"{self.learning_rate} {self.neurons_in_layers[1:-1]} {self.activation_fun.__name__}"
 
 
-def main():
-    mlps = {
-        MLP(1.3, [5], tanh): [],
-        MLP(1.3, [10], tanh): [],
-        MLP(1.3, [70], tanh): [],
-        MLP(1.3, [150], tanh): [],
-    }
-
-    iterations = 0
-    while True:
-        smart_iterator = tqdm(range(20))
-
-        for i in smart_iterator:
-            iterations += 1
+def run_simulation(mlps, iterations):
+    fig_path = f'plots/fig_{datetime.datetime.now()}.png'
+    for i in iterations:
+        smart_iterator = tqdm(range(i), colour='BLUE')
+        smart_iterator.set_postfix_str(f'{i}/{len(iterations)}')
+        for _ in smart_iterator:
             for mlp in mlps.keys():
                 mlp.count_one_step()
                 prediction = mlp.get_predictions()
@@ -123,7 +122,19 @@ def main():
         plt.xlabel("iterations")
         plt.ylabel("accuracy")
         plt.legend()
+        plt.savefig(fig_path)
         plt.show()
+
+
+def main():
+    mlps = {
+        MLP(1.3, [5], tanh): [],
+        MLP(1.3, [10], tanh): [],
+        MLP(1.3, [70], tanh): [],
+        MLP(1.3, [150], tanh): [],
+    }
+    iterations = [10, 5, 5, 5, 10, 10]
+    run_simulation(mlps, iterations)
 
 
 if __name__ == '__main__':
