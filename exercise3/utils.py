dec3673931b9002931cbe082ebd8062626b31261
Kamila Sproska <kamila.sproska@gmail.com>
2022-11-15 12:55:26 +0100

    convnet vs mlp experiments

--- exercise3/utils.py
+++ exercise3/utils.py
@@ -23,12 +23,14 @@ def get_prepared_data():
 
 
 def get_layers_info(model_layers):
-    output = "n\ttype\tfilters\tkernel_size\tactivation\tpool_size\tstrides\n"
-    for i, l in enumerate(model_layers[1:-2]):
+    output = "n\ttype\tfilters\tkernel_size\tactivation\tpool_size\tstrides\tunits\n"
+    for i, l in enumerate(model_layers):
         if type(l) == layers.MaxPooling2D or type(l) == layers.AveragePooling2D:
-            output += "".join([str(i), "\t", l.__class__.__name__, "\t\t\t\t", str(l.pool_size), "\t", str(l.strides)])
+            output += "".join([str(i), "\t", l.__class__.__name__, "\t\t\t\t", str(l.pool_size), "\t", str(l.strides), "\t"])
         if type(l) == layers.Conv2D:
-            output += "".join([str(i), "\t", l.__class__.__name__, "\t", str(l.filters), "\t", str(l.kernel_size), "\t", l.activation.__name__, "\t\t"])
+            output += "".join([str(i), "\t", l.__class__.__name__, "\t", str(l.filters), "\t", str(l.kernel_size), "\t", l.activation.__name__, "\t\t\t"])
+        if type(l) == layers.Dense:
+            output += "".join([str(i), "\t", l.__class__.__name__, "\t\t\t\t\t\t", str(l.units)])
         output += "\n"
     output = output[:-1]
 
