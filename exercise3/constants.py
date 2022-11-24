c7dfbab9ef9e95a30764c3fed8173c5f1d797cf4
Kamila Sproska <kamila.sproska@gmail.com>
2022-11-24 07:50:08 +0100

    prune bad experiments + experiments update

--- exercise3/constants.py
+++ exercise3/constants.py
@@ -1,3 +1,5 @@
+import keras.metrics.metrics
+
 OPT_ADAM = "adam"
 OPT_ADAGRAD = "adagrad"
 OPT_ADADELTA = "adadelta"
@@ -7,3 +9,9 @@ OPT_SGD = "sgd"
 ACT_RELU = "relu"
 ACT_TANH = "tanh"
 ACT_SIGMOID = "sigmoid"
+
+TP = keras.metrics.metrics.TruePositives()
+TN = keras.metrics.metrics.TrueNegatives()
+FP = keras.metrics.metrics.FalsePositives()
+FN = keras.metrics.metrics.FalseNegatives()
+AUC = keras.metrics.metrics.AUC()
